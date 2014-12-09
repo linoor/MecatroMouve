f74e4b61d84faed5f0b93365630f0572f9a00f85
eric.chiu <wf870125@gmail.com>
2014-12-09 17:12:48 +0100

    change from mag to gyro

--- gps+acc+mag/updateData.ino
+++ gps+acc+mag/updateData.ino
@@ -1,8 +1,9 @@
 #include <Wire.h>
-#include "SoftwareSerial.h"
-#include "TinyGPS++.h"
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM303_U.h>
+#include <Adafruit_L3GD20_U.h>
+#include "SoftwareSerial.h"
+#include "TinyGPS++.h"
 
 #define RXPin 52
 #define TXPin 53
@@ -19,6 +20,7 @@ TinyGPSPlus gps;
 // Adafruit_9DOF                dof   = Adafruit_9DOF();
 Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
 Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
+Adafruit_L3GD20_Unified       gyro  = Adafruit_L3GD20_Unified(20);
 
 
 void setup()
@@ -35,7 +37,14 @@ void setup()
     Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
     while(1);
   }
-  Serial.println("Acc+Mag Start");
+  Serial.println("Acc Start");
+  if(!gyro.begin())
+  {
+    /* There was a problem detecting the L3GD20 ... check your connections */
+    Serial.print("Ooops, no L3GD20 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  Serial.println("Gyro Start");
 
   ss.begin(GPSBaud);
   Serial.println("GPS Start");
@@ -45,13 +54,13 @@ void setup()
 
 void loop()
 {
-  float accMag[6];
-  getAccAndMag(accMag);
+  float accGyro[6];
+  getAccAndMag(accGyro);
   float pos[2];
   getGPSPosition(pos);
 
   for (int i = 0; i < 6; i++) {
-    Serial.print(accMag[i]);
+    Serial.print(accGyro[i]);
     Serial.print(',');
   }
   Serial.println();
@@ -65,20 +74,20 @@ void loop()
   delay(200);
 }
 
-void getAccAndMag(float* accMag)
+void getAccAndMag(float* accGyro)
 {
   sensors_event_t eventAcc;
-  sensors_event_t eventMag;
+  sensors_event_t eventGyro;
 
   accel.getEvent(&eventAcc);
-  mag.getEvent(&eventMag);
+  gyro.getEvent(&eventGyro);
 
-  accMag[0] = (float)eventAcc.acceleration.x;
-  accMag[1] = (float)eventAcc.acceleration.y;
-  accMag[2] = (float)eventAcc.acceleration.z;
-  accMag[3] = (float)eventMag.magnetic.x;
-  accMag[4] = (float)eventMag.magnetic.y;
-  accMag[5] = (float)eventMag.magnetic.z;
+  accGyro[0] = eventAcc.acceleration.x;
+  accGyro[1] = eventAcc.acceleration.y;
+  accGyro[2] = eventAcc.acceleration.z;
+  accGyro[3] = eventGyro.gyro.x;
+  accGyro[4] = eventGyro.gyro.y;
+  accGyro[5] = eventGyro.gyro.z;
 }
 
 void getGPSPosition(float* pos)
